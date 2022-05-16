5872311989c3a9ba5e3d44eb6fe5244f0fefa686
Silvio Ankermann <silvio@booq.org>
2022-05-16 22:20:27 +0200

    Fix path in ready-probe.sh

--- ready-probe.sh
+++ ready-probe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if $HOME/bin/ejabberdctl status>/dev/null 2>/dev/null && [ -e $HOME/.ejabberd_ready ]; then
+if ejabberdctl status>/dev/null 2>/dev/null && [ -e $HOME/.ejabberd_ready ]; then
     return 0
 else
     return 3
